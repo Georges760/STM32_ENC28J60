0beb160a63414eb622489f678734a3ac86aa33c9
Georges Palauqui <g.palauqui@gptechinno.com>
2016-01-06 14:38:42 +0100

    Use the ported driver to make a Web server

--- src/main.c
+++ src/main.c
@@ -33,13 +33,37 @@
 
 /* Includes */
 #include "stm32f4xx_hal.h"
+#include "Ethernet.h"
+#include "string.h"
 
 /* Private macro */
+#define MYWWWPORT 80
+#define BUFFER_SIZE 550
 
 /* Private variables */
 SPI_HandleTypeDef SpiHandle;
 
+static uint8_t mymac[6] = { 0x54, 0x57, 0x51, 0x10, 0x00, 0x25 };
+//static uint8_t myip[4] = { 192, 168, 0, 15};
+static uint8_t buf[BUFFER_SIZE + 1];
+uint16_t plen, dat_p;
+
+/* For DHCP */
+static uint8_t myip[4] = { 0, 0, 0, 0 };
+static uint8_t mynetmask[4] = { 0, 0, 0, 0 };
+
+// Default gateway. The ip address of your DSL router. It can be set to the same as
+// websrvip the case where there is no default GW to access the
+// web server (=web server is on the same lan as this host)
+static uint8_t gwip[4] = { 192, 168, 0, 1 };
+
+static uint8_t dnsip[4] = { 0, 0, 0, 0 };
+static uint8_t dhcpsvrip[4] = { 0, 0, 0, 0 };
+
 /* Private function prototypes */
+uint16_t http200ok(void);
+uint16_t print_webpage(uint8_t *buf);
+uint16_t print_webpage2(uint8_t *buf);
 void SystemClock_Config(void);
 static void GPIO_Init(void);
 static void SPI_Init(void);
@@ -60,9 +84,71 @@ int main(void) {
 
 	SPI_Init();
 
-	while (1) {
+	/* Initialize Ethernet */
+	ETH_RST_ON();
+	HAL_Delay(5);
+	Ethernet_Init(mymac);
 
+	if (Ethernet_Revision() <= 0) {
+		// Failed to access ENC28J60
+		while (1);    // Just loop here
 	}
+
+	// Get IP Address details
+	if (Ethernet_allocate_ip_address(buf, BUFFER_SIZE, mymac, 80, myip, mynetmask, gwip, dhcpsvrip, dnsip) > 0) {
+		// Display the results:
+	} else {
+		// Failed to get IP address"
+		while (1);
+	}
+
+	while (1) {
+		// read packet, handle ping and wait for a tcp packet:
+		dat_p = Ethernet_packetloop_icmp_tcp(buf, Ethernet_PacketReceive(BUFFER_SIZE, buf));
+
+		if (dat_p != 0) {
+			if (strncmp("GET ", (char *) &(buf[dat_p]), 4) != 0) {
+				dat_p = http200ok();
+				dat_p = Ethernet_fill_tcp_data(buf, dat_p, "<h1>200 OK</h1>");
+			} else if (strncmp("/ ", (char *) &(buf[dat_p + 4]), 2) == 0) {
+				dat_p = print_webpage(buf);
+			} else if (strncmp("/test ", (char *) &(buf[dat_p + 4]), 2) == 0) {
+				dat_p = print_webpage2(buf);
+			} else {
+				dat_p =	Ethernet_fill_tcp_data(buf, 0, "HTTP/1.0 401 Unauthorized\r\nContent-Type: text/html\r\n\r\n<h1>401 Unauthorized</h1>");
+			}
+			Ethernet_www_server_reply(buf, dat_p);
+		}
+	}
+}
+
+uint16_t http200ok(void) {
+	return (Ethernet_fill_tcp_data(buf, 0, "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nPragma: no-cache\r\n\r\n"));
+}
+
+uint16_t print_webpage(uint8_t *buf) {
+	uint16_t plen;
+	plen = http200ok();
+	plen = Ethernet_fill_tcp_data(buf, plen, "<html><head><title>STM32F4 ENC28J60 Ethernet Demo</title></head><body>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "<center><h1>Welcome to STM32F4 ENC28J60 Ethernet Demo</h1>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "<br> Control digital outputs (not implemented yet)");
+	plen = Ethernet_fill_tcp_data(buf, plen, "<br> Read digital analog inputs (not implemented yet)");
+	plen = Ethernet_fill_tcp_data(buf, plen, "<br></font></h2>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "</center><hr>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "</body></html>");
+	return (plen);
+}
+
+uint16_t print_webpage2(uint8_t *buf) {
+	uint16_t plen;
+	plen = http200ok();
+	plen = Ethernet_fill_tcp_data(buf, plen, "<html><head><title>Test page</title></head><body>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "<center><h1>Second page</h1>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "<br>Hello World");
+	plen = Ethernet_fill_tcp_data(buf, plen, "<br></font></h2>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "</center><hr>");
+	plen = Ethernet_fill_tcp_data(buf, plen, "</body></html>");
+	return (plen);
 }
 
 void SystemClock_Config(void) {
@@ -142,6 +228,42 @@ void SPI_Init(void) {
 	SpiHandle.Init.NSS = SPI_NSS_SOFT;
 	HAL_SPI_Init(&SpiHandle);
 }
+
+void ENC28J60_DelayUs(uint32_t delay) {
+	do {
+		asm volatile (	"MOV R0,%[loops]\n\t"\
+				"1: \n\t"\
+				"SUB R0, #1\n\t"\
+				"CMP R0, #0\n\t"\
+				"BNE 1b \n\t" : : [loops] "r" (20*delay) : "memory"\
+			      ); // test logic analyzer, target 100us : 20->97us 21->127us
+	} while(0);
+}
+
+void ENC28J60_DelayMs(uint32_t delay) {
+	HAL_Delay(delay);
+}
+
+uint32_t ENC28J60_GetMs(void) {
+	return HAL_GetTick();
+}
+
+void ENC28J60_EnableChip(void) {
+	ETH_NSS_SELECT();
+}
+
+void ENC28J60_DisableChip(void) {
+	ETH_NSS_RELEASE();
+}
+
+uint8_t ENC28J60_TransceiveByte(uint8_t data) {
+	uint8_t received;
+	if (HAL_SPI_TransmitReceive(&SpiHandle, &data, &received, 1, 1000) == HAL_OK) {
+		return received;
+	}
+	return 0;
+}
+
 
 #ifdef USE_FULL_ASSERT
 
